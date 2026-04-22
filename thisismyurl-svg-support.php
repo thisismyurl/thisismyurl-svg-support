4eba7a0e8e0411d81077b9f1d4cece086bba6b5f
thisismyurl <actions@users.noreply.github.com>
2026-04-22 14:59:17 +0000

    Release 0.6112

--- thisismyurl-svg-support.php
+++ thisismyurl-svg-support.php
@@ -6,7 +6,7 @@
  * Plugin URI:  https://thisismyurl.com/thisismyurl-svg-support/
  * Donate link: https://thisismyurl.com/donate/
  * Description: Safely enable SVG uploads and management in the WordPress Media Library.
- * Version:     1.251229
+ * Version:     0.6112
  * Requires at least: 6.0
  * Requires PHP: 7.4
  * Update URI: https://github.com/thisismyurl/thisismyurl-svg-support
